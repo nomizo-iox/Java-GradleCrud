297b584ea5e3de5d4892c068d0b7a40a7d6ff27e
nomizo-iox <damascus@Samuels-MacBook-Pro.local>
2020-04-10 19:55:07 -0500

    fixed issues with enum not recognizing string-char issues.

--- src/main/java/com/nomizo/controller/ClientController.java
+++ src/main/java/com/nomizo/controller/ClientController.java
@@ -4,9 +4,7 @@ package com.nomizo.controller;
 import com.nomizo.model.Client;
 import com.nomizo.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,18 @@ public class ClientController {
     ClientService clientService;
 
     @GetMapping("/clients")
-    public List<Client> getClients () { return clientService.findClientss(); }
+    public List<Client> getClients() { return clientService.findClientss(); }
+
+    @GetMapping("/client/id/{id}")
+    public Client getClientById(@PathVariable int id) { return clientService.findClientById(id); }
+
+    @GetMapping("/client/email/{email}")
+    public Client getClientByEmail(@PathVariable String email) { return clientService.findClientByEmail(email); }
+
+    @PostMapping("/addClient")
+    public Client postClient(@RequestBody Client client) { return clientService.saveClient(client); }
+
+    @PostMapping("/addClients")
+    public List<Client> postClients(@RequestBody List<Client> clients) { return clientService.saveClients(clients); }
+
 }
